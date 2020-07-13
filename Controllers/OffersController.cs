91bf567d6b9f3eb036770dee96a722e6e752afc8
taifaraby1 <ihsan.faraabi@gmail.com>
2020-07-13 15:05:36 +0700

    Code Cleaning

--- Controllers/OffersController.cs
+++ Controllers/OffersController.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using MerchantApp.Data;
+using MerchantApp.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
-using MerchantApp.Data;
-using MerchantApp.Models;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace MerchantApp.Controllers
 {
